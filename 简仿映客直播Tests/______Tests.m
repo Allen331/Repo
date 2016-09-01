3bb8be9200a2267d33c0a227c50491f28dbbdb16
Allen <495544258@qq.com>
2016-09-01 18:05:04 +0800

    all

---
+++ 简仿映客直播Tests/______Tests.m
@@ -0,0 +1,39 @@
+//
+//  ______Tests.m
+//  简仿映客直播Tests
+//
+//  Created by apple on 16/6/22.
+//  Copyright © 2016年 下载链接 http://pan.baidu.com/s/1kUR5GcJ. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ______Tests : XCTestCase
+
+@end
+
+@implementation ______Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
